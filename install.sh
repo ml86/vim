2987f4d991a21cf3884f487522c56a1cec1601b7
Markus Lottmann <markus@mlbox.net>
2016-12-28 22:09:41 +0100

    Fix and generalization.

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > tmp_install.sh
 sh ./tmp_install.sh
-m tmp_install.sh
-ln -s ~/.vim/.vimrc ~/.vimrc
+rm tmp_install.sh
+ln -s $(pwd)/$(dirname $0)/.vimrc ~/.vimrc
